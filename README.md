416174be2467dba76b49ea6beb6eb2da7249b75a
Florent Biville <fbiville@pivotal.io>
2020-05-20 16:54:44 +0200

    Push example

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Deploying an unmanaged extension to `Neo4jContainer`
+
+## Reproduction
+
+To reproduce the problem, just run:
+
+```shell
+$ mvn test
+```
+
+The test will fail.
+
+> You can change the Neo4j version via `-Dneo4j.version`. The goal is for this to work across all Neo4j **3.x** versions.
+
+## Container inspection
+
+Suspend the execution of `io.github.fbiville.testcontainers.neo4j.ClearDatabaseExtensionTest.unmanaged_extension_is_called` (set a breakpoint at the beginning for instance).
+Then:
+
+1. find the running container via `docker ps`
+```
+CONTAINER ID        IMAGE                               COMMAND                  CREATED             STATUS              PORTS                                                                       NAMES
+61fb36dbdc2f        neo4j:3.0.0                         "/docker-entrypoint.…"   11 seconds ago      Up 10 seconds       0.0.0.0:33511->7473/tcp, 0.0.0.0:33510->7474/tcp, 0.0.0.0:33509->7687/tcp   heuristic_dewdney
+3e1e00c9ebcb        quay.io/testcontainers/ryuk:0.2.3   "/app"                   12 seconds ago      Up 11 seconds       0.0.0.0:33508->8080/tcp                                                     testcontainers-ryuk-eb8da151-b1a2-4122-be75-608cae2bcb45
+```
+1. start an interactive shell session via `docker exec -it ${CONTAINER_ID} /bin/bash` (in the example, `CONTAINER_ID` == `61fb36dbdc2f`)
+1. check the environment, notice that the expected unmanaged setting is there:
+```
+root@61fb36dbdc2f:/var/lib/neo4j# env | grep unmanaged
+NEO4J_dbms_unmanaged__extension__classes=io.github.fbiville.testcontainers.neo4j=/ext
+```
+1. check the configuration, notice it has not been changed and does **not** enable the unmanaged extension
+```
+root@61fb36dbdc2f:/var/lib/neo4j# grep unmanaged conf/neo4j.conf
+# org.neo4j.examples.server.unmanaged.HelloWorldResource.java from
+# neo4j-server-examples under /examples/unmanaged, resulting in a final URL of
+# http://localhost:7474/examples/unmanaged/helloworld/{nodeId}
+#dbms.unmanaged_extension_classes=org.neo4j.examples.server.unmanaged=/examples/unmanaged
+```
---
+++ test/src/test/java/io/github/fbiville/testcontainers/neo4j/ClearDatabaseExtensionTest.java
@@ -0,0 +1,96 @@
+package io.github.fbiville.testcontainers.neo4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import okhttp3.Credentials;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.utility.MountableFile;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClearDatabaseExtensionTest {
+
+	private static final String ADMIN_PASSWORD = "j4eon";
+
+	private static final Neo4jContainer<?> CONTAINER = new Neo4jContainer<>(String.format("neo4j:%s", neo4jVersion()))
+			.withAdminPassword(ADMIN_PASSWORD)
+			.withPlugins(MountableFile.forHostPath(extensionPath()))
+			.withNeo4jConfig("dbms.unmanaged_extension_classes", String
+					.format("%s=/ext", ClearDatabaseExtension.class.getPackage().getName()));
+
+	private static final OkHttpClient httpClient = new OkHttpClient.Builder()
+			.authenticator((route, response) -> response.request()
+					.newBuilder()
+					.header("Authorization", Credentials.basic("neo4j", ADMIN_PASSWORD))
+					.build())
+			.build();
+
+	@BeforeClass
+	public static void prepare_all() {
+		CONTAINER.start();
+	}
+
+	@AfterClass
+	public static void clean_up_all() {
+		CONTAINER.stop();
+	}
+
+	@Test
+	public void unmanaged_extension_is_called() throws IOException {
+		String url = String.format("%s/ext/clearDb", CONTAINER.getHttpUrl());
+		Request request = new Request.Builder()
+				.url(url)
+				.header("Accept", "application/json")
+				.post(RequestBody.create("", MediaType.parse("application/json")))
+				.build();
+
+		try (Response response = httpClient.newCall(request).execute()) {
+			assertThat(response.isSuccessful())
+					.describedAs(String
+							.format("Expected to successfully call unmanaged extension at URL: %s but got status: %d", url, response
+									.code()))
+					.isTrue();
+		}
+	}
+
+	private static String neo4jVersion() {
+		return readSingleLine("/neo4j.version");
+	}
+
+	private static String extensionPath() {
+		return readSingleLine("/extension.location");
+	}
+
+
+	private static String readSingleLine(String classpathResource) {
+		List<String> lines = readLines(classpathResource);
+		int lineCount = lines.size();
+		if (lineCount != 1) {
+			throw new RuntimeException(String
+					.format("%s (filtered) resource should contain exactly 1 line, found: %d", classpathResource, lineCount));
+		}
+		return lines.iterator().next();
+	}
+
+	private static List<String> readLines(String classpathResource) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(ClearDatabaseExtensionTest.class
+				.getResourceAsStream(classpathResource)))) {
+			return reader.lines().collect(Collectors.toList());
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
